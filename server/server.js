7d1beb02d39cebe2f9461db491fc5ca4659f4edd
mvgrds15-eng <mvgrds15@gmail.com>
2026-04-15 16:39:53 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,15 +3,18 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/tokenDB')
+// IMPORTANT FIX 👇
+mongoose.connect(process.env.MONGO_URL)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
-const tokenRoutes = require('./routes/tokenRoutes');
-app.use('/api', tokenRoutes);
+app.get("/", (req, res) => {
+  res.send("API Running");
+});
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
