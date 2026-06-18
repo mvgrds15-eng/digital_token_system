6300a92fe8b0d7a016a70876b1a21de9e0c07d4d
mvgrds15-eng <mvgrds15@gmail.com>
2026-06-18 15:11:30 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,24 +1,40 @@
+require('dotenv').config();
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
-// ✅ ADD THIS LINE
-const tokenRoutes = require('./routes/tokenRoutes');
+const tokenRoutes =
+require('./routes/tokenRoutes');
+
 app.use('/api', tokenRoutes);
 
-// MongoDB connection
-mongoose.connect(process.env.MONGO_URL)
-  .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.log(err));
+mongoose.connect(
+process.env.MONGO_URL
+)
+.then(() =>
+console.log("MongoDB Connected"))
+.catch(err =>
+console.log(err));
 
-app.get("/", (req, res) => {
-  res.send("API Running");
+app.get("/", (req,res)=>{
+
+res.send("Digital Token API Running");
+
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+const PORT =
+process.env.PORT || 3000;
+
+app.listen(PORT, ()=>{
+
+console.log(
+`Server running on port ${PORT}`
+);
+
 });
